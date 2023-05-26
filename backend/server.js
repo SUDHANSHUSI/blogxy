b42f025ee8a631188cb5f2a68fc18cfa9c7a91ed
UserOne <sudhanshusingh0401@gmail.com>
2023-05-26 16:02:08 +0530

    added cors

--- backend/server.js
+++ backend/server.js
@@ -15,6 +15,18 @@ dbConnect();
 
 app.use("/api", userRoutes);
 
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept,Authorization"
+  );
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,PATCH,POST,DELETE,OPTIONS');
+  next();
+});
+
+
 app.all("*", (req, res, next) => {
   return next(
     new AppError(`can't find ${req.originalUrl} on this server!`, 404)
